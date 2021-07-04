78e9db480fa2ca9eac00b5fa3b995792c4597010
seovalue <seominjeong.dev@gmail.com>
2021-07-04 21:39:45 +0900

    feat: 공통 정보를 가지는 BaseEntity 생성

---
+++ src/main/java/qna/domain/BaseEntity.java
@@ -0,0 +1,24 @@
+package qna.domain;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
